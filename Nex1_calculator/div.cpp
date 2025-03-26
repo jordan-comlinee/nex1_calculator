e2c55c3b821e1c1d4ab5267ed28685c2040886f6
kim-jinuk <88013805+kim-jinuk@users.noreply.github.com>
2025-03-26 15:51:25 +0900

    Update div.cpp

--- Nex1_calculator/div.cpp
+++ Nex1_calculator/div.cpp
@@ -1,7 +1,7 @@
 
 T div(const T& a, const T& b) {
     if (b ==  0) {
-        std:: cout << "김진욱 바보" << std :: endl ;
+        std:: cout << "김동혁 바보" << std :: endl ;
         return -999999;
     }
         return a / b;
