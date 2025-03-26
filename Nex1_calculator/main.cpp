2b1cca443f6249958e2360c1b9b529e8d53872f2
Donghyukleeee <ewqr9808@gmail.com>
2025-03-25 23:34:42 -0700

    <feat> testfunction to main

--- Nex1_calculator/main.cpp
+++ Nex1_calculator/main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 
+
+#include "div.h"
+
 using namespace std;
 
 int main() {
@@ -8,4 +11,9 @@ int main() {
 	a = 500;
 	b = 25;
 	// 각자 구현한 함수로 사칙연산 진행
+
+	double divTest1 = 2.5 , divTest2 = 1.2;
+	cout << "div(divTest1,divTets2)" << endl;
+	cout << div(divTest1,divTets2) << endl;
+
 }
