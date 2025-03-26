cc48fb96d3ac91b6528bfee78f471cdb80756bbb
heroPark19 <dnd8848@naver.com>
2025-03-25 23:32:56 -0700

    hero

--- Nex1_calculator/main.cpp
+++ Nex1_calculator/main.cpp
@@ -2,10 +2,15 @@
 
 using namespace std;
 
+template<typename T>
+
+T plus(T a, T b){
+	retrun a + b;
+}
+
 int main() {
 	cout << "Hello, Calculator!" << endl;
 	int a, b;
 	a = 500;
 	b = 25;
-	// 각자 구현한 함수로 사칙연산 진행
 }
